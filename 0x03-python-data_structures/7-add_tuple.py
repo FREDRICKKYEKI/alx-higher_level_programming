74ca6d19028653df4b732a683c8781387609b391
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-05-13 11:46:45 +0300

    pep8 correction

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -21,12 +21,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = (a1 + b1, a2 + b2)
     
     return (new_tuple)
-
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
