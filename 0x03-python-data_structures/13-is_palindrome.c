213764d1c1ae92c4c19f9781c57faf127dc5e498
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-05-13 12:52:17 +0300

    advanced tasks

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,113 @@
+#include "lists.h"
+/**
+ * reverse - reverses the second half of the list
+ *
+ * @h: head of the second half
+ * Return: void
+ */
+void reverse(listint_t **h)
+{
+	listint_t *prev;
+	listint_t *curr;
+	listint_t *next;
+
+	prev = NULL;
+	curr = *h;
+
+	while (curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+
+	*h = prev;
+}
+
+/**
+ * compare - compares each int of the list
+ *
+ * @h1: head of the first half
+ * @h2: head of the second half
+ * Return: 1 if are equals, 0 if not
+ */
+int compare(listint_t *h1, listint_t *h2)
+{
+	listint_t *temp1;
+	listint_t *temp2;
+
+	temp1 = h1;
+	temp2 = h2;
+
+	while (temp1 != NULL && temp2 != NULL)
+	{
+		if (temp1->n == temp2->n)
+		{
+			temp1 = temp1->next;
+			temp2 = temp2->next;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	if (temp1 == NULL && temp2 == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list
+ * is a palindrome
+ * @head: pointer to head
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow, *fast, *prev_slow;
+	listint_t *scn, *middle;
+	int is_palind;
+
+	slow = fast = prev_slow = *head;
+	middle = NULL;
+	is_palind = 1;
+
+	if (*head != NULL && (*head)->next != NULL)
+	{
+		while (fast != NULL && fast->next != NULL)
+		{
+			fast = fast->next->next;
+			prev_slow = slow;
+			slow = slow->next;
+		}
+
+		if (fast != NULL)
+		{
+			middle = slow;
+			slow = slow->next;
+		}
+
+		scn = slow;
+		prev_slow->next = NULL;
+		reverse(&scn);
+		is_palind = compare(*head, scn);
+
+		if (middle != NULL)
+		{
+			prev_slow->next = middle;
+			middle->next = scn;
+		}
+		else
+		{
+			prev_slow->next = scn;
+		}
+	}
+
+	return (is_palind);
+}
