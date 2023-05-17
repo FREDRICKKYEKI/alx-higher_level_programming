94919ac02e97fbfabe7bae6db3e1107a5b552b26
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-05-17 17:41:52 +0300

    [fix]: sq. matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def square_matrix(matrix=[]):
-    len_o = len(matrix)
-    len_i = len(matrix[0])
-    new_mat = [[] for i in range(len_o)]
-    for i in range(len_o):
-        for j in range(len_i):
-            new_mat[i].append(matrix[i][j] ** 2)
+def square_matrix_simple(matrix=[]):
+    new_mat = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_mat[i] = list(map(lambda x: x**2, matrix[i]))
+
     return (new_mat)
