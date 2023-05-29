38b1b9c72268d5160529d880f1fb82a785445c3a
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-05-29 16:27:15 +0300

    [fix]: safe print int

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value), end="")
+        print("{:d}".format(value))
     except Exception as e:
         sys.stderr.write("Exception: {}\n".format(e))
         return False
