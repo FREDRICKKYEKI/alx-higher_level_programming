40ef7473ae9ae6984c03f35ece0cc6b8d9b4cffc
Fredrick Kyeki <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-05-30 19:58:48 +0300

    Update 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -31,7 +31,7 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if type(value) != Node:
+        if type(value) != Node and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
