bc1f733340c5b9ba3baa0caeb8d4a0d4266160bb
Fredrick Kyeki <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-05-30 19:00:46 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+python module which defines a class square
+"""
 class Square:
-    """ An empty class called square
+    """
+    An empty class called square
     """
     pass
