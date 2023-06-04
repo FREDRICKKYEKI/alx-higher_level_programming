26d5beda52b399465b209294357147ce4c2abf01
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2023-06-04 22:59:07 +0300

    final commit

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -6,9 +6,13 @@ module that has the lazy_matrix_multiply function
 that multiplies 2 matrices
 
 """
+
+
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
+
     """Function that multiply 2 matrices
     Args:
         m_a (list): matrix a
