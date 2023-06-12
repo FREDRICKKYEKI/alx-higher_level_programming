fa0df7964d30e55b1aed2d40cd784852c400de64
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-06-12 19:29:00 +0300

    [fix]: 8-rect

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 
 module that contains the Rectangle class that
@@ -7,6 +6,8 @@ extends the BaseGeometry class
 
 """
 
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
