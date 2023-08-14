ed767711e4d1ee84852b7959a32eec0b5ecfe8f0
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-08-14 16:56:06 +0300

    [update]: clean task 4

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -5,4 +5,3 @@ const strArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < strArr.length; i++) {
   console.log(strArr[i]);
 }
-
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
-//Write a script that prints x times “C is fun”
-
-const times = new Number(process.argv[2]);
+const times = Number(process.argv[2]);
 const str = 'C is fun';
 
-// if is a number and exists
 if (times && !isNaN(times)) {
-  for (let i = 0; i < times; i++)
-    console.log(str);
+  for (let i = 0; i < times; i++) { console.log(str); }
 } else {
   console.log('Missing number of occurrences');
 }
