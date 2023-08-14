73ba27cc3bc292e0d82a7c46ff8d6783b8139614
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-08-14 19:43:11 +0300

    [fix]: task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,11 +7,11 @@ function printSquare (size) {
   for (let i = 0; i < size; i++) {
     w = '';
     for (let j = 0; j < size; j++) {
-      w += 'x';
+      w += 'X';
     }
     console.log(w);
   }
 }
 
-if (!Number.isNaN(size)) printSquare(size);
+if (size && !Number.isNaN(size)) printSquare(size);
 else console.log('Missing size');
