13264af4eddc17e707317917c29ae548890a25c0
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-08-14 19:46:36 +0300

    [fix]: task 102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,20 +1,8 @@
 #!/usr/bin/node
 
-/**
- * Write a function that increments and calls a function.
-
-The function must be visible from outside
-Prototype: function (number, theFunction)
-You are not allowed to use var
- */
 
 function addMeMaybe (number, theFunction) {
-  let j = 0;
-
-  for (let i = 0; i <= number; i++) j = i;
-  j++;
-
-  theFunction(j);
+  theFunction(number + 1);
 }
 
 module.exports.addMeMaybe = addMeMaybe;
