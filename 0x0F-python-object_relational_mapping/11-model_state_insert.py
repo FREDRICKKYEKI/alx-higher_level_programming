3bcb02a68458dee48dcf18d28c628dfefc14b7bf
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-08-19 23:50:07 +0300

    [fix]: pycodestyle compliance

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
- - Write a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+ - Write a script that adds the State object “Louisiana” to the database
+   hbtn_0e_6_usa
 
- - Your script should take 3 arguments: mysql username, mysql password and database name
+ - Your script should take 3 arguments: mysql username, mysql password and
+   database name
  - You must use the module SQLAlchemy
- - You must import State and Base from model_state - from model_state import Base, State
- - Your script should connect to a MySQL server running on localhost at port 3306
+ - You must import State and Base from model_state -
+   from model_state import Base, State
+ - Your script should connect to a MySQL server running on localhost at
+   port 3306
  - Print the new states.id after creation
  - Your code should not be executed when imported
 """
@@ -29,6 +33,5 @@ if __name__ == '__main__':
 
     session.add(new_state)
     session.commit()
-
 
     print(new_state.id)
