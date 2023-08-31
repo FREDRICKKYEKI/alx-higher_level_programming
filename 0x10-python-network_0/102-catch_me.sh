21216890142bcfd35db62b39816cc0d9e52f7ba7
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-09-01 00:13:32 +0300

    [fix]: fix (task 5)

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me the server to respond
-curl -s -X PUT -d "user_id=98" "0.0.0.0:5000/catch_me"
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:ALXSchool"
