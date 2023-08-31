73407172d50402c83ca1d427fc56e044779ba80d
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-09-01 00:03:20 +0300

    [fix]: fix 3 line requirement

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Write a Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-#   - The size must be displayed in bytes
-#   - You have to use curl
-# Please test your script in the sandbox provided, using the web server
-# running on port 5000
-
+# takes in a URL, sends a request to that URL nd displays the body
 curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-# Write a Bash script that takes in a URL as an argument, sends a GET
-# request to the URL, and displays the body of the response
-
-# A header variable X-School-User-Id must be sent with the value 98
-# You have to use curl
-
+# sends a GET request, displays the body of the response
 curl -s "$1" -H "X-School-User-Id: 98"
