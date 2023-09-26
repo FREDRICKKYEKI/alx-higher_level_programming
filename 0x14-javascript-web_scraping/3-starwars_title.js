368e8a9a8a6946104f391c11ff4d03971b6f783f
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-09-26 17:34:13 +0300

    [fix]: fix#3 task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -10,11 +10,12 @@
 const request = require('request');
 
 const id = process.argv[2];
-// const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request('http://swapi.co/api/films/' + id + '/', (err, resp, body) => {
-  if (err == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
   }
 });
