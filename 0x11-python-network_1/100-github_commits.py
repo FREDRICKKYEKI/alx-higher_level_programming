acf37a4adb1b3bf9d2cab12ba24209a2f2bd9527
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-09-26 11:25:44 +0300

    [update]: save before pulling

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,15 +14,14 @@ from requests import get, auth
 import sys
 
 
-if __name__ == "__main__":
-    try:
-        repo = sys.argv[1]
-        owner = sys.argv[2]
-        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-        req = get(url)
-        json = req.json()
-        for i in range(0, 10):
-            print("{}: {}".format(json[i].get('sha'), json[i].get('commit')
-                                  .get('author').get('name')))
-    except Exception as e:
-        pass
+try:
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    req = get(url)
+    json = req.json()
+    for i in range(0, 10):
+        print("{}: {}".format(json[i].get('sha'), json[i].get('commit')
+                              .get('author').get('name')))
+except Exception as e:
+    pass
